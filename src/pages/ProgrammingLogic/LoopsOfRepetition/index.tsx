4ab18711a32b1dbbaf05cb254c73f51a4ac20886
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-14 20:54:42 -0300

    Finalizando a page Loops

--- src/pages/ProgrammingLogic/LoopsOfRepetition/index.tsx
+++ src/pages/ProgrammingLogic/LoopsOfRepetition/index.tsx
@@ -5,6 +5,7 @@ export const LoopRepetition = () => {
     <Container>
       <Header>
         <h1>Laços de Repetição</h1>
+
         <p>
           Afim de fazer operações repetitivas, o JavaScript oferece algumas
           opções de estruturas de repetições. Também chamados de laços ou loops.
@@ -33,14 +34,19 @@ export const LoopRepetition = () => {
 
         <p>
           A condição dada para a execução é count menor do que 100. Assim
-          enquanto essa condição retornar true, o código será executado. Na
-          linha 4 há a atualização da variável count, o ++ representa a operação
-          em aumentar em 1 o valor da variável. Note que se não houvesse essa
-          linha a condição nunca se tornaria falsa, uma vez que continuaria
-          tendo valor 0. Neste caso o laço seria infinito. Preste sempre atenção
-          para evitar esse tipo de cenário. Note também que se a condição for
-          falsa logo de início, por exemplo se mudarmos a linha 1 para let count
-          = 200;, o código dentro do while nunca seria executado.
+          enquanto essa condição retornar true, o código será executado.
+          <br />
+          Na linha 4, há a atualização da variável count, o ++ representa a
+          operação em aumentar em 1 o valor da variável.
+          <br />
+          Note que se não houvesse essa linha a condição nunca se tornaria
+          falsa, uma vez que continuaria tendo valor 0. Neste caso o laço seria
+          infinito.
+          <br />
+          Preste sempre atenção para evitar esse tipo de cenário. Note também,
+          que se a condição for falsa logo de início, por exemplo se mudarmos a
+          linha 1 para let count = 200; , o código dentro do while nunca seria
+          executado.
         </p>
       </Section>
 
