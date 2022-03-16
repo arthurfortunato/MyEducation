5a63bae6dcaf729d848a47a8b8ca57d1df8f9e03
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-15 22:54:37 -0300

    Estilizando o conteúdo de funções

--- src/pages/ProgrammingLogic/Functions/index.tsx
+++ src/pages/ProgrammingLogic/Functions/index.tsx
@@ -9,33 +9,43 @@ export const Functions = () => {
 
       <Section className="section section__functions">
         <p>
-          Podemos ter duas interpretações do que são funções. A primeira de que
-          uma função é um trecho de código em que damos um nome e que é
-          executado a cada vez que é chamado. Exemplo:
+          Podemos ter duas interpretações do que são <strong>funções</strong>. A
+          primeira de que uma função é{" "}
+          <strong>
+            um trecho de código em que damos um nome e que é executado a cada
+            vez que é chamado
+          </strong>
+          . Exemplo:
         </p>
 
         <Pre>
           <code>
             {`
-           // Definindo a função
-           function geradorNumerico(){
-               for(let i = 0; i < 10; i++){
-                   console.log(i);
-               }
-           }
- 
-           // Chamando a função
-           geradorNumerico();
-         `}
+              // Definindo a função
+
+              function geradorNumerico(){
+                  for(let i = 0; i < 10; i++) {
+                      console.log(i);
+                  }
+              }
+    
+              // Chamando a função
+              geradorNumerico();
+          `}
           </code>
         </Pre>
 
+        <br />
+        <br />
         <p>
           Criamos uma função usando a function e demos o nome geradorNumerico. É
           uma função que apenas imprime no console os número de 0 a 100. Cada
-          vez que é chamada a função, o seu código é executado. Para tornar a
-          função mais reutilizável, podemos parametriza-la. Para isso,
-          adicionamos os parâmetros na declaração.
+          vez que é chamada a função, o seu código é executado. <br />
+          <strong>
+            {" "}
+            Para tornar a função mais reutilizável, podemos parametriza-la
+          </strong>
+          . Para isso, adicionamos os parâmetros na declaração.
         </p>
 
         <Pre>
@@ -53,9 +63,11 @@ export const Functions = () => {
           `}
           </code>
         </Pre>
+        <br />
+        <br />
 
         <p>
-          Entre os parêntesis adicionamos a declaração de duas variáveis, comeco
+          Entre os parêntesis adicionamos a declaração de duas variáveis, começo
           e fim. De forma que a função imprime os números entre esses dois
           valores.
         </p>
@@ -68,14 +80,16 @@ export const Functions = () => {
         <Pre>
           <code>
             {`
-            function soma(a,b){
-              return a+b;
+            function soma(a, b){
+              return a + b;
           }
         
-            const resultado = soma(2,3);
+            const resultado = soma(2, 3);
          `}
           </code>
         </Pre>
+        <br />
+        <br />
 
         <p>
           Declaramos dois parâmetros, a e b, que ao ser passados a função soma,
@@ -98,23 +112,32 @@ export const Functions = () => {
           <code>
             {`
               // Usando function
-              function soma1(a,b) { return a+b; }
+              function soma1(a, b) { 
+                return a + b; 
+              }
+
               // Atribuindo uma função anônima em constantes
-              const soma2 = function(a,b){ return a+b; };
+              const soma2 = function(a, b){ 
+                return a+b; 
+              }
+
               // Atribuindo uma função de "flecha" em constantes
-              const soma3 = (a,b) => a+b;
+              const soma3 = (a,b) => a + b;
          `}
           </code>
         </Pre>
+        <br />
+        <br />
 
         <p>
-          A primeira função foi declarada como a forma tradicional apresentada
-          antes. No segundo caso foi usado uma função anônima, isto é, sem nome.
-          Note que seguido de function não há declaração de um nome para a
-          função. O que define o seu nome é a variável. No terceiro caso é
-          também uma função anônima porém é usada a chamada função de flecha.
-          Onde é declarado os parâmetros de entrada em parêntesis e a sua
-          definição seguido da flecha {`=>`}.
+          A primeira função foi declarada como a{" "}
+          <strong>forma tradicional</strong> apresentada antes. No segundo caso
+          foi usado uma <strong>função anônima</strong>, isto é, sem nome. Note
+          que seguido de function não há declaração de um nome para a função. O
+          que define o seu nome é a variável. No terceiro caso é também uma
+          função anônima porém é usada a chamada{" "}
+          <strong>função de flecha</strong>. Onde é declarado os parâmetros de
+          entrada em parêntesis e a sua definição seguido da flecha {`=>`}.
         </p>
 
         <p>Exemplos de funções de flecha:</p>
@@ -123,11 +146,15 @@ export const Functions = () => {
           <code>
             {`
                 const hello1 = () => "Hello World!";
+
                 const hello2 = (name) => "Hello " + name;
+                
                 const hello3 = (name) => {return "Hello " + name;};
           `}
           </code>
         </Pre>
+        <br />
+        <br />
 
         <p>
           Note que não é possível fazer qualquer restrição de tipo de entrada
