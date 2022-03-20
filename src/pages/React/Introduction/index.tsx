c430aba22d63744cc5ad5489ff3753ee677e893c
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-20 10:49:28 -0300

    iniciando o conteúdo de React

---
+++ src/pages/React/Introduction/index.tsx
@@ -0,0 +1,18 @@
+export const IntroductionReact = () => {
+  return (
+    <main>
+      <header>
+        <h1>Introdução ao React</h1>
+      </header>
+
+      <section>
+        <p>
+          <h2>Inicializando com React</h2>
+          O React é uma biblioteca JavaScript declarativa, eficiente e flexível
+          para criar interfaces com o usuário. Ele permite compor UIs complexas
+          a partir de pequenos e isolados códigos chamados “componentes”.
+        </p>
+      </section>
+    </main>
+  );
+};
