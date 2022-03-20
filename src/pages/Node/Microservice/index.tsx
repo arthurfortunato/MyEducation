36e8aa29129f9d593b83b0ca28d553de9f07d170
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-19 21:54:17 -0300

    Estilizando o conteúdo de Microservice

--- src/pages/Node/Microservice/index.tsx
+++ src/pages/Node/Microservice/index.tsx
@@ -10,18 +10,22 @@ export const Microservice = () => {
       <Section className="section section__microservice">
         <h2>O que são Microsserviços?</h2>
         <p>
-          Quando alguém fala em microsserviços, está falando basicamente em uma
-          arquitetura. A Arquitetura de Microsserviços.
+          <strong>
+            Quando alguém fala em microsserviços, está falando basicamente em
+            uma arquitetura. A Arquitetura de Microsserviços.
+          </strong>
           <br />
           <br />
           Com eles, as aplicações são desmembradas em componentes mínimos e
           independentes.
           <br />
           <br />
-          Diferentemente da abordagem tradicional monolítica em que toda a
-          aplicação é criada como um único bloco, os microsserviços são
-          componentes separados que trabalham juntos para realizar as mesmas
-          tarefas.
+          <strong>
+            Diferentemente da abordagem tradicional monolítica em que toda a
+            aplicação é criada como um único bloco, os microsserviços são
+            componentes separados que trabalham juntos para realizar as mesmas
+            tarefas.
+          </strong>
           <br />
           <br />
           Cada um dos componentes ou processos é um microsserviços. A vertente
@@ -52,26 +56,34 @@ export const Microservice = () => {
       <Section className="section section__api-microservice">
         <h2>A diferença entre API e microsserviços</h2>
         <p>
-          Tanto a API como os microsserviços são camadas dentro de um software.
-          Muitas vezes os conceitos se sobrepõe, por isso é importante entender
-          que a API é uma camada criada para comunicação externa.
+          <strong>
+            Tanto a API como os microsserviços são camadas dentro de um
+            software. Muitas vezes os conceitos se sobrepõe, por isso é
+            importante entender que a API é uma camada criada para comunicação
+            externa.
+          </strong>
           <br />
           <br />
           Ou seja, pode-se dizer que escalar o software por meio de outros
           softwares é um dos seus objetivos.
           <br />
-          <br />A API é a camada do meu software responsável pela troca de dados
-          entre qualquer outro software externo. Já os microsserviços, que
-          também são camadas, tem um intuito diferente: escalar o software
-          internamente, facilitando implantação, proteção e manutenção de
-          features.
+          <br />
+          <strong>
+            A API é a camada do meu software responsável pela troca de dados
+            entre qualquer outro software externo. Já os microsserviços, que
+            também são camadas, tem um intuito diferente: escalar o software
+            internamente, facilitando implantação, proteção e manutenção de
+            features.
+          </strong>
         </p>
       </Section>
 
       <Section className="section section__why-microservice">
         <h2>Quando e por que usar microsserviços?</h2>
-        <h3>Definição clara de domínios</h3>
 
+        <h3>
+          <strong>Definição clara de domínios</strong>
+        </h3>
         <p>
           Se você possui um microsserviço responsável pelo usuário, ele será a
           única referência para mexer em um dado do usuário, ou performar
@@ -82,8 +94,12 @@ export const Microservice = () => {
           usuário, ele tem que estritamente buscá-lo a partir do microsserviço
           de usuário.
         </p>
+        <br />
+        <br />
 
-        <h3>Escalabilidade</h3>
+        <h3>
+          <strong>Escalabilidade</strong>
+        </h3>
         <p>
           Quando quebramos uma aplicação monolítica e grande em várias pequenas,
           possivelmente conseguimos escalá-las de forma separada.
@@ -99,17 +115,20 @@ export const Microservice = () => {
           monolítica.
           <br />
           <br />
-          <h3>A diversificação de tecnologia</h3>
-          <p>
-            Os microsserviços não necessariamente precisam ser escritos usando a
-            mesma linguagem de programação.
-            <br />
-            <br />
-            Se cabe o uso de Python em determinado microsserviço, digamos que
-            seja algo ligado a processamento de dados, por que não usar?
-            <br />
-            <br />
-          </p>
+        </p>
+
+        <h3>
+          <strong>A diversificação de tecnologia</strong>
+        </h3>
+        <p>
+          Os microsserviços não necessariamente precisam ser escritos usando a
+          mesma linguagem de programação.
+          <br />
+          <br />
+          Se cabe o uso de Python em determinado microsserviço, digamos que seja
+          algo ligado a processamento de dados, por que não usar?
+          <br />
+          <br />
         </p>
       </Section>
     </Container>
