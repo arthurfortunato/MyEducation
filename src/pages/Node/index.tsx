487513f2050b7828619a33f1202d43abe2b35eb6
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-24 09:42:36 -0300

    Adicionando um mapa mental ao node

--- src/pages/Node/index.tsx
+++ src/pages/Node/index.tsx
@@ -1,18 +1,22 @@
 import { Container } from "./styles";
+import node from "../../assets/NODEJS.png";
 
 export const Node = () => {
   return (
     <Container>
-      <h1>NODE.JS</h1>
+      <main>
+        <h1>NODE.JS</h1>
 
-      <p>
-        <strong>
-          O Node.js é ideal para aplicações multidirecionais que ocorrem em
-          tempo real, como chats.{" "}
-        </strong>
-        Além disso, pode ser usado na transmissão e visualização de dados, assim
-        como no acompanhamento das interações da audiência no seu site.
-      </p>
+        <p>
+          <strong>
+            O Node.js é ideal para aplicações multidirecionais que ocorrem em
+            tempo real, como chats.{" "}
+          </strong>
+          Além disso, pode ser usado na transmissão e visualização de dados,
+          assim como no acompanhamento das interações da audiência no seu site.
+        </p>
+        <img src={node} alt="Mapa mental node" />
+      </main>
     </Container>
   );
 };
--- src/pages/Node/styles.ts
+++ src/pages/Node/styles.ts
@@ -2,54 +2,59 @@ import styled from "styled-components";
 
 export const Container = styled.main`
   height: 100vh;
+  width: 100%;
+  background: #fff;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background: #fff;
-  padding-top: 80px;
 
-  h1 {
-    text-align: center;
-    font: 700 4rem "Lato", sans-serif;
-  }
+  main {
+    width: 70%;
 
-  p {
-    width: 60%;
-    font: 400 1.3rem "Lato", sans-serif;
-    margin: 10px;
-    text-align: justify;
-    line-height: 1.7rem;
-  }
-
-  strong {
-    color: #f5b324;
-  }
-
-  @media (max-width: 920px) {
     h1 {
-      transition: 1.8s all;
-      font-size: 2.5rem;
+      text-align: center;
+      font: 700 4rem "Lato", sans-serif;
     }
+
     p {
-      transition: 1.8s all;
-      font-size: 1rem;
-      tex-align: left;
+      font: 400 1.3rem "Lato", sans-serif;
+      margin: 10px;
+      text-align: justify;
+      line-height: 1.7rem;
     }
-  }
 
-  @media (max-width: 790px) {
-    h1 {
-      font-size: 2rem;
+    strong {
+      color: #f5b324;
     }
-    p {
-      width: 80%;
-    }
-  }
 
-  @media (max-width: 500px) {
-    h1 {
-      font-size: 1.5rem;
+    img {
+      width: 100%;
+      height: 85%;
+    }
+
+    @media (max-width: 920px) {
+      h1 {
+        transition: 1.8s all;
+        font-size: 2.5rem;
+      }
+      p {
+        transition: 1.8s all;
+        font-size: 1rem;
+        tex-align: left;
+      }
+    }
+
+    @media (max-width: 790px) {
+      h1 {
+        font-size: 2rem;
+      }
+    }
+
+    @media (max-width: 500px) {
+      h1 {
+        font-size: 1.5rem;
+      }
     }
   }
 `;
