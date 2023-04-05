270ccccf333f46f818c31a9790f21bfb32eeef71
arthurfortunato <luizarthur.fortunato@gmail.com>
2023-04-05 17:26:28 -0300

    commit routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import { HomeRoutes } from "./homeRoutes";
 import { ConceptsRoutes } from "./concepts";
-import { ProgrammingLogicRoutes } from "./programmingLogicRoutes";
-import { PooRoutes } from "./pooRoutes";
-import { NodeRoutes } from "./nodeRoutes";
-import { JavaRoutes } from "./javaRoutes";
-import { ReactRoutes } from "./reactRoutes";
-import { DockerRoutes } from "./dockerRoutes";
 
 export const Routers: any = () => {
   return (
