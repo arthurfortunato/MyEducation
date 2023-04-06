89c4ba198589e59d907990d3af1d834819419b4d
arthurfortunato <luizarthur.fortunato@gmail.com>
2023-04-06 15:54:38 -0300

    fix full screen

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
-  
+
   return (
     <ThemeProvider theme={isDarkMode ? theme.dark : theme.light}>
       <ThemeMode onClick={toggleDarkMode}>
