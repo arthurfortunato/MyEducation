57855d0bfed93180e205b1762dc543fca5e72e77
arthurfortunato <luizarthur.fortunato@gmail.com>
2023-04-06 16:38:21 -0300

    fix full screen

--- src/pages/Start/index.tsx
+++ src/pages/Start/index.tsx
@@ -1,7 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Container, ButtonStart } from "./styles";
 
 export const Start = () => {
+  const navigate = useNavigate();
+
+  const getStarted = () => {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    const fullScreenEnabled =
+      document.fullscreenEnabled ||
+      (document as any).webkitFullscreenEnabled ||
+      (document as any).mozFullScreenEnabled ||
+      (document as any).msFullscreenEnabled ||
+      false;
+
+    if (isMobile && fullScreenEnabled) {
+      document.documentElement.requestFullscreen();
+    }
+
+    navigate("/home")
+  };
+  
   return (
     <Container>
       <Link to="/home" className="link-start">
