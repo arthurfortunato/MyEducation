65736c416acea1421f8e49891f546a7327c0ac31
arthurfortunato <luizarthur.fortunato@gmail.com>
2023-04-06 13:46:12 -0300

    fix mobile navigator

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,26 @@ function App() {
     setIsDarkMode(!isDarkMode);
   };
 
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const fullScreenEnabled =
+    document.fullscreenEnabled ||
+    (document as any).webkitFullscreenEnabled ||
+    (document as any).mozFullScreenEnabled ||
+    (document as any).msFullscreenEnabled ||
+    false;
+  
+  if (isMobile && fullScreenEnabled) {
+    document.documentElement.requestFullscreen();
+  }  
+
   return (
     <ThemeProvider theme={isDarkMode ? theme.dark : theme.light}>
       <ThemeMode onClick={toggleDarkMode}>
-        {isDarkMode ? <MdDarkMode size={32} /> : <MdLightMode size={32} />}
+        {isDarkMode ? (
+          <MdDarkMode size={32} />
+        ) : (
+          <MdLightMode size={32} color="#000" />
+        )}
       </ThemeMode>
       <GlobalStyled />
       <Routers />
