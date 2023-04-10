2a16a6df7c973921ec212238f25786c219d25cc6
arthurfortunato <luizarthur.fortunato@gmail.com>
2023-04-10 10:21:33 -0300

    adding content java

---
+++ src/pages/Java/JDK/paragraphs.tsx
@@ -0,0 +1,27 @@
+export const paragraphs = [
+  <strong>
+    O Java Development Kit (JDK) é um conjunto de ferramentas de desenvolvimento
+    para a linguagem de programação Java.
+  </strong>,
+  <p>
+    Ele é composto de diversas ferramentas e utilitários, incluindo o compilador
+    de Java, o depurador, o javadoc (gerador de documentação), entre outros.
+  </p>,
+
+  <strong>
+    O JDK é utilizado por desenvolvedores para criar, compilar e executar
+    aplicativos em Java. Ele é necessário para desenvolver aplicativos Java,
+    pois contém todas as ferramentas necessárias para compilar e executar código
+    Java.
+  </strong>,
+
+  <p>
+    Além disso, o JDK é uma plataforma independente, o que significa que os
+    aplicativos desenvolvidos nele podem ser executados em qualquer plataforma
+    que suporte o Java, desde que a máquina virtual Java (JVM) esteja instalada.
+  </p>,
+  <p>
+    O JDK é disponibilizado gratuitamente pela Oracle Corporation, que é a
+    mantenedora oficial da linguagem de programação Java.
+  </p>,
+];
